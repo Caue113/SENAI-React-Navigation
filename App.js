efd658c9449d2223a78562896a55a3a3890f8250
Luis Felipe Torres Saturnino <luisftsat@gmail.com>
2022-08-24 11:26:50 -0300

    tutorial - 1st part
    Troubleshooting encountered. Might delay development

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native-web';
+import { createStackNavigator, createAppContainer} from "react-navigation";
 
+import HomeScreen from './components/HomeScreen';
+import AboutScreen from './components/AboutScreen';
+
+/*
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer/>
   );
+}*/
+
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
 }
+
+
+
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  About: {
+    screen: AboutScreen
+  }
+}, {
+  initialRouteName: "Home"
+});
+
+const AppContainer = createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
